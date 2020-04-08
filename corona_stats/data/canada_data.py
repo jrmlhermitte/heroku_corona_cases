0021acc277e412b053d9db00a6902774384c940b
julien <julien@useriver.com>
2020-04-07 22:18:12 -0400

    added comment

--- corona_stats/data/canada_data.py
+++ corona_stats/data/canada_data.py
@@ -1,3 +1,10 @@
+"""
+Canada data pulled from:
+    https://www.canada.ca/en/public-health/services/diseases/2019-novel
+    -coronavirus-infection.html
+"""
+
+
 import urllib.request
 import datetime as dt
 
